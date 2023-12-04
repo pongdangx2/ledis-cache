1039adc9d332b6d19bed645ffb6e6f0a5bf0ff93
lkhzz <leta6603@gmail.com>
2023-12-04 19:47:11 +0900

    initial commit

---
+++ src/test/java/me/lkh/lediscache/LedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lkh.lediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
