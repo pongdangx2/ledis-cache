4f91cd002bcfb69287fa93e47fcac0d634f3abd3
lkhzz <leta6603@gmail.com>
2023-12-05 17:41:04 +0900

    여러개의 데이터를 저장/조회하는 기능 추가

--- src/main/java/me/lkh/lediscache/core/ValueLedisCacheTemplate.java
+++ src/main/java/me/lkh/lediscache/core/ValueLedisCacheTemplate.java
@@ -3,13 +3,14 @@ package me.lkh.lediscache.core;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.lkh.lediscache.core.domain.value.FromOrigin;
+import me.lkh.lediscache.core.domain.value.MultipleFromOrigin;
 import me.lkh.lediscache.core.domain.value.ToOrigin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Redis String value를 캐싱하는 모듈
@@ -31,7 +32,7 @@ public class ValueLedisCacheTemplate<K, V> {
     }
 
     /**
-     * 데이터 조회 (Look Aside Cache)
+     * 하나의 데이터 조회 - 캐시를 먼저 조회하고 없으면 원본 저장소 조회
      * @param key 조회할 Key
      * @param valueClass 조회할 Value의 타입
      * @param fromOrigin 캐시 miss 시 원본 데이터를 조회하는 기능을 담당하는 인터페이스
@@ -123,5 +124,90 @@ public class ValueLedisCacheTemplate<K, V> {
             return false;
         }
         return true;
+    }
+
+    /**
+     * 여러 key의 데이터를 조회 - 캐시를 먼저 조회하고 없으면 원본 저장소 조회
+     * @param keySet 조회할 key의 Set
+     * @param valueClass 조회할 Value의 타입
+     * @param multipleFromOrigin 캐시 Miss된 key들을 원본 저장소에서 조회
+     * @return 조회한 (key, value) Map
+     * @throws JsonProcessingException
+     * @see me.lkh.lediscache.core.domain.value.MultipleFromOrigin
+     */
+    public Map<K, V> getValue(Set<K> keySet, Class<V> valueClass, MultipleFromOrigin multipleFromOrigin) throws JsonProcessingException {
+
+        // 1. redis 캐시 조회
+        Map<K, V> result = getValueFromRedis(new ArrayList<>(keySet), valueClass);
+
+        // 2. cache miss 된 key만 따로 보관
+        Set<K> originKeySet = new HashSet<>();
+        keySet.forEach(key -> {
+            if(!result.containsKey(key)){
+                originKeySet.add(key);
+            }
+        });
+
+        // 3. cache miss된 데이터 원본 저장소에서 조회 및 캐시에 저장할 것들 필터링
+        Map<String, String> cacheMap = new HashMap<>();
+        Map<K, V> originMap = multipleFromOrigin.getMultiDataFromOrigin(originKeySet);
+        for(K key: originMap.keySet()){
+            V value = originMap.get(key);
+
+            result.put(key, (V) value);
+            cacheMap.put(objectMapper.writeValueAsString(key), objectMapper.writeValueAsString(value));
+        }
+
+        // 4. MultiSet
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.multiSet(cacheMap);
+
+        return result;
+    }
+
+    private Map<K, V> getValueFromRedis(List<K> keyList, Class<V> valueClass) throws JsonProcessingException {
+        Map<K, V> result = new HashMap<>();
+
+        // 1. keyList의 element를 String으로 변환
+        List<String> stringKeyList = new ArrayList<>();
+        for(K key : keyList){
+            stringKeyList.add(objectMapper.writeValueAsString(key));
+        }
+
+        // 2. redis multiGet
+        ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+        List<String> values = stringValueOperations.multiGet(stringKeyList);
+
+        // 3. 존재하는 것만 결과에 추가
+        int len = keyList.size();
+        for(int i = 0; i < len; i++){
+            String value = values.get(i);
+            if(value != null){
+                V tmpValue = objectMapper.readValue(value, valueClass);
+                result.put(keyList.get(i), tmpValue);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * multiSet을 이용해 여러개의 key,value 데이터를 캐시 및 원본 데이터에 저장
+     * @param data 저장할 (key, value) Map
+     * @param toOrigin 원본 저장소에 데이터를 저장
+     * @throws JsonProcessingException
+     */
+    public void setValue(Map<K, V> data, ToOrigin toOrigin) throws JsonProcessingException {
+
+        if(toOrigin.saveToOrigin()){
+            Map<String, String> stringMap = new HashMap<>();
+            for(K key : data.keySet()){
+                stringMap.put(objectMapper.writeValueAsString(key), objectMapper.writeValueAsString(data.get(key)));
+            }
+
+            ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+            stringValueOperations.multiSet(stringMap);
+        }
+
     }
 }
--- src/main/java/me/lkh/lediscache/core/domain/value/FromOrigin.java
+++ src/main/java/me/lkh/lediscache/core/domain/value/FromOrigin.java
@@ -1,8 +1,6 @@
 package me.lkh.lediscache.core.domain.value;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 /**
  * 캐시에 데이터가 존재하지 않을 경우, 원본데이터를 가져오는 역할을 추상화한 인터페이스
@@ -21,13 +19,4 @@ public interface FromOrigin {
      */
     <K, V> Optional<V> getDataFromOrigin(K key, Class<V> valueClass);
 
-    /**
-     * 여러 개의 원본 데이터를 조회하여 반환하는 Method
-     * @param keySet 조회할 Key의 집합
-     * @param valueClass 조회할 Value의 타입
-     * @param <K> key의 generic
-     * @param <V> value의 generic
-     * @return keySet에 있는 key와 key 로 조회한 Value의 Map (원본에 미존재 시 반환되는 Map 에는 포함되지 않아야 한다.)
-     */
-    <K, V> Map<K, V> getMultiDataFromOrigin(Set<K> keySet, Class<V> valueClass);
 }
